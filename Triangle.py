102e60c0c3b02fe6343646a0257c67826fe3cd0f
VishweshMS <55580232+VishweshMS@users.noreply.github.com>
2023-02-10 19:32:44 -0500

    update Triangle.py
    updated Triangle.py to fix the existing bugs

--- Triangle.py
+++ Triangle.py
@@ -7,7 +7,7 @@ The primary goal of this file is to demonstrate a simple python program to class
 
 @author: Vishwesh Malur Somashekar
 """
-
+import datetime
 def classifyTriangle(a,b,c):
     """
     Your correct code goes here...  Fix the faulty logic below until the code passes all of 
@@ -30,7 +30,7 @@ def classifyTriangle(a,b,c):
     if a > 200 or b > 200 or c > 200:
         return 'InvalidInput'
         
-    if a <= 0 or b <= b or c <= 0:
+    if a <= 0 or b <= 0 or c <= 0:
         return 'InvalidInput'
     
     # verify that all 3 inputs are integers  
@@ -42,15 +42,21 @@ def classifyTriangle(a,b,c):
     # is important for correctness
     # the sum of any two sides must be strictly less than the third side
     # of the specified shape is not a triangle
-    if (a >= (b - c)) or (b >= (a - c)) or (c >= (a + b)):
+    if (a >= (b + c)) or (b >= (a + c)) or (c >= (a + b)):
         return 'NotATriangle'
         
     # now we know that we have a valid triangle 
     if a == b and b == a:
         return 'Equilateral'
-    elif ((a * 2) + (b * 2)) == (c * 2):
+    elif  (a ** 2) + (b ** 2) == (c ** 2) or (a ** 2) + (c ** 2) == (b ** 2) or (b ** 2) + (c ** 2) == (a ** 2):
         return 'Right'
-    elif (a != b) and  (b != c) and (a != b):
+    elif (a != b) and  (b != c) and (a != c):
         return 'Scalene'
     else:
         return 'Isoceles'
+
+def my_brand(assignment_name):
+    print("=*=*=*= Vishwesh Malur Somashekar =*=*=*=")
+    print("=*=*=*= Course 2023S-SSW567-WS =*=*=*=")
+    print("=*=*=*= " + assignment_name + " =*=*=*=")
+    print("=*=*=*= " + str(datetime.datetime.now()) + " =*=*=*=")
